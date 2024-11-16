ece40c181cee7bacc6deeb489b0b84cb4c655bb0
San Thit Aung <43712985+S4NTH174UN9@users.noreply.github.com>
2024-11-16 22:27:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+app.config["SESSION_TYPE"] = "null"
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
