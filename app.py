c688acc3df44cf254f54968c158b9f163e5a0b4b
San Thit Aung <43712985+S4NTH174UN9@users.noreply.github.com>
2024-11-16 22:34:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from flask_sqlalchemy import SQLAlchemy
 from helpers import apology, login_required, lookup, usd
 
 # Configure application
@@ -14,8 +14,11 @@ app = Flask(__name__)
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "null"
+app.config["SESSION_TYPE"] = "sqlalchemy"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///session.db"
+db = SQLAlchemy(app)
+
+Session(app)
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
